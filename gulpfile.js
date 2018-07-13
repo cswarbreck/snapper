a26839f6d1b136937af03c4dfdb27796bcb397ba
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-07-13 19:36:54 +0100

    removed comments from code

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,6 @@ var cache = require('gulp-cache');
 var del = require('del');
 var runSequence = require('run-sequence').use(gulp);
 
-
-// Browser reloading tasks
-
 gulp.task('sass', function(){
     return gulp.src('app/site.scss')
     .pipe(sass())
@@ -31,23 +28,13 @@ gulp.task('browser-sync', function(){
     });
 });
 
-// This is the master control task for browser reloading
-
 gulp.task('watch', ['browser-sync', 'sass'], function(){
     gulp.watch('app/**/*.scss', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/**/*.js', browserSync.reload);
 });
 
-// In keeping with best practices, the watch task is usually
-// Set to 'default' using the runsequence plugin.
-// For reference, tasks named 'default' can be called by
-// Simply typing $ gulp in the CLI
-
-
 gulp.task('default', ['watch']);
-
-
 
 gulp.task('bootstrap', function() {
     gulp.src('./node_modules/bootstrap/scss/**/*.scss')
@@ -55,11 +42,7 @@ gulp.task('bootstrap', function() {
 
     gulp.src('./node_modules/bootstrap/js/dist/**/*.js')
         .pipe(gulp.dest('./app/js/bootstrap'));
-
 });
-
-// The following tasks will create the production files
-// and send them to the dist folder.
 
 gulp.task('useref', function(){
     return gulp.src('app/*.html')
@@ -81,9 +64,6 @@ gulp.task('fonts', function(){
     return gulp.src('app/fonts/**/*')
   .pipe(gulp.dest('dist/fonts'));
 });
-
-// This clean task will clear out the dist folder of 
-// Unwanted or obsolete files previously compiled
 
 gulp.task('clean:dist', function(){
     return del.sync('dist');
